602df704b599150dd120e832d24626633b9ddf6e
praveen05git <praveenmail@protonmail.com>
2020-12-06 11:16:11 +0530

    Designed new login

--- app/src/main/java/com/hencesimplified/praveenassignment/view/LoginActivity.java
+++ app/src/main/java/com/hencesimplified/praveenassignment/view/LoginActivity.java
@@ -1,9 +1,17 @@
 package com.hencesimplified.praveenassignment.view;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -11,53 +19,101 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.hencesimplified.praveenassignment.R;
 import com.hencesimplified.praveenassignment.viewmodel.AuthViewModel;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LoginActivity extends AppCompatActivity {
 
-    private TextView username;
-    private TextView password;
-    private Button login;
-    private Button registerButton;
+    private TextView loginEmailId;
+    private TextView loginPassword;
+    private Button loginButton;
+    private TextView createAccount;
     private RadioGroup userType;
     private ProgressBar loadingView;
+    private static CheckBox showHidePassword;
+    private static LinearLayout loginLayout;
+    private static Animation shakeAnimation;
     private AuthViewModel authViewModel;
     private String TYPE_FLAG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.acitivity_test);
 
-        username = findViewById(R.id.userName);
-        password = findViewById(R.id.password);
-        login = findViewById(R.id.login);
-        registerButton = findViewById(R.id.registerButton);
+        loginEmailId = findViewById(R.id.loginEmailId);
+        loginPassword = findViewById(R.id.loginPassword);
+        loginButton = findViewById(R.id.loginButton);
+        createAccount = findViewById(R.id.createAccount);
+        showHidePassword = findViewById(R.id.showHidePassword);
+        loginLayout = findViewById(R.id.loginLayout);
         userType = findViewById(R.id.userType);
         loadingView = findViewById(R.id.loadingView);
         loadingView.setVisibility(View.GONE);
+        shakeAnimation = AnimationUtils.loadAnimation(this, R.anim.shake);
 
         authViewModel = new ViewModelProvider(this).get(AuthViewModel.class);
         authViewModel.refresh();
 
-        userType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                switch (checkedId) {
-                    case R.id.doctorType:
-                        TYPE_FLAG = "Doctor";
-                        break;
-                    case R.id.patientType:
-                        TYPE_FLAG = "Patient";
-                        break;
-                }
+        showHidePassword.setOnCheckedChangeListener((button, isChecked) -> {
+
+            if (isChecked) {
+                showHidePassword.setText("Hide Password");
+                loginPassword.setInputType(InputType.TYPE_CLASS_TEXT);
+                loginPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            } else {
+                showHidePassword.setText("Show Password");
+                loginPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                loginPassword.setTransformationMethod(PasswordTransformationMethod
+                        .getInstance());
             }
         });
 
-        login.setOnClickListener(v -> authViewModel.loginWithEmail(username.getText().toString(), password.getText().toString()));
+        userType.setOnCheckedChangeListener((group, checkedId) -> {
+            switch (checkedId) {
+                case R.id.doctorType:
+                    TYPE_FLAG = "Doctor";
+                    break;
+                case R.id.patientType:
+                    TYPE_FLAG = "Patient";
+                    break;
+            }
+        });
 
-        registerButton.setOnClickListener(v -> startActivity(new Intent(this, RegistrationActivity.class)));
+        loginButton.setOnClickListener(v -> {
+
+            String getEmailId = loginEmailId.getText().toString().trim();
+            String getPassword = loginPassword.getText().toString().trim();
+
+            String regEx = "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}\\b";
+
+            Pattern p = Pattern.compile(regEx);
+
+            Matcher m = p.matcher(getEmailId);
+
+            if (getEmailId.equals("") || getEmailId.length() == 0 || getPassword.equals("") || getPassword.length() == 0) {
+
+                loginLayout.startAnimation(shakeAnimation);
+                Snackbar.make(v, "Enter Email and Password ", Snackbar.LENGTH_SHORT)
+                        .setBackgroundTint(Color.parseColor("#96bb7c"))
+                        .setTextColor(Color.parseColor("#ffffff")).show();
+
+            } else if (!m.find()) {
+
+                Snackbar.make(v, "Enter valid Email", Snackbar.LENGTH_SHORT)
+                        .setBackgroundTint(Color.parseColor("#96bb7c"))
+                        .setTextColor(Color.parseColor("#ffffff")).show();
+
+            } else {
+                authViewModel.loginWithEmail(loginEmailId.getText().toString(), loginPassword.getText().toString());
+            }
+        });
+
+        createAccount.setOnClickListener(v -> startActivity(new Intent(this, RegistrationActivity.class)));
 
         observeViewModel();
     }
